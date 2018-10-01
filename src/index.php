6691743afd71537faec4b40cce0015838c45eec0
Nikita <i79307046480@gmail.com>
2018-10-01 20:07:34 +0300

    basics of CURL

--- src/index.php
+++ src/index.php
@@ -3,6 +3,10 @@
 	$ch = curl_init('https://yandex.ru/pogoda/saint-petersburg');
 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, false); // Получаем заголовки
+	//curl_setopt($ch, CURLOPT_NOBODY, true); // Получаем только заголовки
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);// Отключаем проверки, позволяющие 
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);//загружать странички без https
 	$html = curl_exec($ch);
 	curl_close($ch);
 
